5453ad2bb8aa3da6bd3c4feb77a1d569219e0ef0
billchiang <billchiang@fucotech.com.tw>
2019-05-30 18:53:34 +0800

    fix searchreq.go

--- http/searchreq.go
+++ http/searchreq.go
@@ -9,43 +9,41 @@ import (
 )
 
 var searchReqHandler = withUser(func(w http.ResponseWriter, r *http.Request, d *data) (int, error) {
-	// response := []interface{}{}
 	query := r.URL.Query().Get("query")
-	var filesResponse []*files.FileInfo
+	var items []*files.FileInfo
 
 	err := search.Search(d.user.Fs, r.URL.Path, query, d, func(path string, f os.FileInfo) error {
 
-		//search result using NewFileInfo
-		file, _ := files.NewFileInfo(files.FileOptions{
+		items = append(items, &files.FileInfo{
 			Fs:      d.user.Fs,
-			Path:    "/" + f.Name(),
+			Path:    r.URL.Path,
+			Name:    path,
+			ModTime: f.ModTime(),
+			Mode:    f.Mode(),
+			IsDir:   f.IsDir(),
+			Size:    f.Size(),
+		})
+
+		return nil
+	})
+	if len(items) > 0 {
+		searchReq, _ := files.NewFileInfo(files.FileOptions{
+			Fs:      d.user.Fs,
+			Path:    r.URL.Path,
 			Modify:  false,
 			Expand:  true,
 			Checker: d,
 		})
 
-		if file != nil {
-			filesResponse = append(filesResponse, file)
+		searchReq.Listing.Items = items
+		searchReq.Listing.Sorting = d.user.Sorting
+		searchReq.NumDirs = 1
+		searchReq.NumFiles = len(items)
+		searchReq.Listing.ApplySort()
+		if err != nil {
+			return http.StatusInternalServerError, err
 		}
-		return nil
-	})
-	//gen a dir from root to
-	dirab, _ := files.NewFileInfo(files.FileOptions{
-		Fs:      d.user.Fs,
-		Path:    "/",
-		Modify:  false,
-		Expand:  true,
-		Checker: d,
-	})
-	dirab.Listing.Items = filesResponse
-	dirab.Listing.Sorting = d.user.Sorting
-	dirab.NumDirs = 1
-	dirab.NumFiles = len(dirab.Listing.Items)
-	dirab.Listing.ApplySort()
-
-	if err != nil {
-		return http.StatusInternalServerError, err
+		return renderJSON(w, r, searchReq)
 	}
-
-	return renderJSON(w, r, dirab)
+	return renderJSON(w, r, items)
 })
